8d5d2a5b4d83a119381f230cb3900046a86e8b94
Nick K <mnfork@gmail.com>
2024-11-15 14:57:40 -0700

    adding a qwerty version

--- keymap-qwerty.c
+++ keymap-qwerty.c
@@ -6,11 +6,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //Colemak base layer
   [0] = LAYOUT(
   //-------------------------------------------------------------------                       --------------------------------------------------------------------
-    KC_NO, KC_Q,        KC_W,        KC_F,        KC_P,        KC_B,                          KC_J,    KC_L,        KC_U,        KC_Y,        KC_QUOT,      KC_NO,
+    KC_NO, KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,                          KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,      KC_NO,
   //-------------------------------------------------------------------                       --------------------------------------------------------------------
-    KC_NO, GUI_T(KC_A), ALT_T(KC_R), CTL_T(KC_S), SFT_T(KC_T), KC_G,                          KC_M,    SFT_T(KC_N), CTL_T(KC_E), ALT_T(KC_I), GUI_T(KC_O),  KC_NO,
+    KC_NO, GUI_T(KC_A), ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,                          KC_H,    SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN),  KC_NO,
   //-------------------------------------------------------------------                       --------------------------------------------------------------------
-    KC_NO, KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,    KC_ENT,      KC_TAB,  KC_K,    KC_H,        KC_COMM,     KC_DOT,      KC_SLSH,      KC_NO,
+    KC_NO, KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,    KC_ENT,      KC_TAB,  KC_N,    KC_H,        KC_COMM,     KC_DOT,      KC_SLSH,      KC_NO,
   //-------------------------------------------------------------------                       --------------------------------------------------------------------
                                                   MO(2),       MO(1),   QK_GESC,     KC_BSPC, KC_SPC, QK_REP
   ),
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //Number layer
   [2] = LAYOUT(
   //-------------------------------------------------------------------                       --------------------------------------------------------------------
-    KC_NO, KC_PSCR,     KC_INS,      KC_DEL,      RCS(KC_M),   KC_VOLU,                       KC_PEQL, KC_7,        KC_8,        KC_9,         KC_PPLS,     KC_NO,
+    KC_NO, KC_PSCR,     KC_INS,      KC_DEL,      RCS(KC_M),   KC_VOLU,                       KC_EQL, KC_7,        KC_8,        KC_9,         KC_PPLS,     KC_NO,
   //-------------------------------------------------------------------                       --------------------------------------------------------------------
     KC_NO, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_VOLD,                       KC_PAST, KC_4,        KC_5,        KC_6,         KC_PMNS,     KC_NO,
   //-------------------------------------------------------------------                       --------------------------------------------------------------------

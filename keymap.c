7fb1b5aff8c37334fe8d85b5bf8923741dec5560
Nick K <mnfork@gmail.com>
2025-02-19 17:22:32 -0700

    adding momentary navigation

--- keymap.c
+++ keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //------------------------------------------------------------------                       -----------------------------------------------------------------------
     KC_NO, KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,   KC_TAB,      TO(3),   KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,         KC_NO,
   //------------------------------------------------------------------                       -----------------------------------------------------------------------
-                                                  MO(2),       MO(1),  QK_GESC,     KC_BSPC, KC_SPC,  KC_ENT
+                                                  MO(2),       MO(1),  QK_GESC,     KC_BSPC, LT(3, KC_SPC),  KC_ENT
   ),
 
   //Symbol layer
